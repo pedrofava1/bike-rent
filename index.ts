27c0b9e4d652cb955c86215eae43e72650586548
Pedro Fava <pedrofava2004@gmail.com>
2023-08-28 17:13:26 -0300

    new configs

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { App } from "./src/app"
+import { Bike } from "./src/bike"
+import { Rent } from "./src/rent"
+import { User } from "./src/user"
+
+const bike = new Bike('bikel', 'mountain', 30, 100, 100.5, 'my desc', 5, [])
+const user = new User ('teste', 'teste@mail.com', '1234')
+const today = new Date()
+const twoDaysLater = new Date()
+twoDaysLater.setDate(twoDaysLater.getDate() + 2)
+const tomorrow = new Date()
+tomorrow.setDate(tomorrow.getDate() + 1)
+const twoDaysAfterTomorrow = new Date()
+twoDaysAfterTomorrow.setDate(tomorrow.getDate() + 2)
+const rent1 = Rent.create([], bike, user, today, twoDaysLater)
+const rent2 = Rent.create([rent1], bike, user, tomorrow, twoDaysAfterTomorrow)
+
+console.log(rent2);
+
+const app = new App()
+const ul = new User('Joao', 'joao@email.com', '1234')
+app.addUser(ul)
+console.log(ul);
+
+app.addUser(ul)
+
